25dd7178a1028623a26141bb5049cc9ce5053a36
Evgenii <60140529+fliker2309@users.noreply.github.com>
2021-11-16 16:47:21 +0300

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.39.1")
+    }
+}
+plugins {
+    id("io.gitlab.arturbosch.detekt") version "1.18.1"
+    id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
+}
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+}
+
+dependencies {
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.18.1")
+}
+
+detekt {
+    toolVersion = "1.10.0"
+    config = files("config/detekt/detekt.yml")
+    buildUponDefaultConfig = true
+
+    reports {
+        html {
+            enabled = true
+            destination = file("app/build/detekt/detekt.html")
+        }
+    }
+}
+
+task("clean") {
+    doLast {
+        delete(rootProject.buildDir)
+    }
+}
