05f30d46b76c2279167dbe886a8cf4a5dd3f5fd5
Evgenii <60140529+fliker2309@users.noreply.github.com>
2021-11-16 18:09:24 +0300

    fix:delete useless icons refactor:rename fragments

--- app/src/main/java/com/example/simplerecipes/presentation/ui/favourites/FavouritesFragment.kt
+++ app/src/main/java/com/example/simplerecipes/presentation/ui/favourites/FavouritesFragment.kt
@@ -1,4 +1,4 @@
-package com.example.simplerecipes.presentation.ui.dashboard
+package com.example.simplerecipes.presentation.ui.favourites
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,12 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.simplerecipes.databinding.FragmentDashboardBinding
+import com.example.simplerecipes.databinding.FragmentFavouritesBinding
 
-class DashboardFragment : Fragment() {
+class FavouritesFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
-    private var _binding: FragmentDashboardBinding? = null
+    private lateinit var favouritesViewModel: FavouritesViewModel
+    private var _binding: FragmentFavouritesBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,16 +24,19 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        favouritesViewModel =
+            ViewModelProvider(this).get(FavouritesViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentFavouritesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val textView: TextView = binding.textNotifications
+        favouritesViewModel.text.observe(
+            viewLifecycleOwner,
+            Observer {
+                textView.text = it
+            }
+        )
         return root
     }
 
--- app/src/main/java/com/example/simplerecipes/presentation/ui/favourites/FavouritesViewModel.kt
+++ app/src/main/java/com/example/simplerecipes/presentation/ui/favourites/FavouritesViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.simplerecipes.presentation.ui.dashboard
+package com.example.simplerecipes.presentation.ui.favourites
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class FavouritesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is favourites Fragment"
     }
     val text: LiveData<String> = _text
 }
